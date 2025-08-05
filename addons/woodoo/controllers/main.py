4c47ee78819c519ef4a11cf7df273077f2f72c39
Harkály Gergő <git@harkalygergo.hu>
2025-08-05 19:20:17 +0200

    /hello URL works too

--- addons/woodoo/controllers/main.py
+++ addons/woodoo/controllers/main.py
@@ -4,7 +4,7 @@ class HelloWorld(http.Controller):
     # This route makes the 'hello' method accessible at '/hello'
     @http.route('/hello', type='http', auth='public', website=True)
     def hello(self, **kw):
-        return http.request.render('hello_world_template')
+        return http.request.render('woodoo.hello_world_template')
 
     # You can also return a simple string without a template
     @http.route('/hello/string', auth='public')
