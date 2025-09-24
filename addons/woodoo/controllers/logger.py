f937ad545961c14a0f2df25d393069019e100f0a
Harkály Gergő <git@harkalygergo.hu>
2025-09-24 15:30:02 +0200

    add DEBUG parameter to dotenv and log only if it is true

--- addons/woodoo/controllers/logger.py
+++ addons/woodoo/controllers/logger.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 
+load_dotenv(dotenv_path="config/.env")
 
 class Logger():
     @staticmethod
     def log(message):
+        debug = os.getenv("DEBUG", "false").lower() == "true"
+        if not debug:
+            return
         with open("/tmp/woodoo.log", "a") as log_file:
             log_file.write("\n=====================\n")
             log_file.write(f"{datetime.now()}\n")
