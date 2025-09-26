14f023fe4d8f4b1abee5fa46da957a8a6c83b000
Harkály Gergő <git@harkalygergo.hu>
2025-09-26 10:52:22 +0200

    relocate product_template.py and modify wordpress ID var name

--- addons/woodoo/__manifest__.py
+++ addons/woodoo/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'WooDoo',
-    'version': '18.0.20250924.8',
+    'version': '18.0.20250924.9',
     'summary': 'WooDoo is an Odoo ERP module that syncs data between Odoo and WordPress+WooCommerce webshop.',
     'category': 'Sales',
     'author': 'Harkály Gergő',
--- addons/woodoo/models/odoo2woo/product_template.py
+++ addons/woodoo/models/odoo2woo/product_template.py
@@ -12,16 +12,16 @@ class ProductTemplate(models.Model):
         productTemplate = super(ProductTemplate, self).write(vals)
 
         for record in self:
-            default_code = record.default_code if record.default_code else ""
+            wordpress_product_id = record.default_code if record.default_code else ""
             # check if default_code is not empty
-            if default_code:
+            if wordpress_product_id:
                 try:
-                    Logger.log(f"Updating product {default_code} with vals: {json.dumps(vals, indent=4)}")
+                    Logger.log(f"Updating product {wordpress_product_id} with vals: {json.dumps(vals, indent=4)}")
                     # log all data of record and vals
                     Logger.log("RECORD:" + json.dumps(record.read()[0], indent=4, default=str, ensure_ascii=False))
 
                     data = {
-                        "id": default_code,
+                        "id": wordpress_product_id,
                         "name": str(vals.get('name', record.name)),
                         "regular_price": str(vals.get('list_price', record.list_price)),
                         "description": str(vals.get('description_ecommerce', record.description_ecommerce)),
@@ -31,7 +31,7 @@ class ProductTemplate(models.Model):
                     Logger.log(json.dumps(data, indent=4))  # Pretty print the data being sent
 
                     wooAPI = WooAPI.get(self)
-                    response = wooAPI.put(f"products/{default_code}", data)
+                    response = wooAPI.put(f"products/{wordpress_product_id}", data)
                     if response.status_code != 200:
                         Logger.log("API error:" + response.status_code + response.text)
                     else:
